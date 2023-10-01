9a5b7a34b1690d46c8ac72f1fa839ab154f9d828
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-01 18:13:00 +0300

    Added CMakeLists.txt with two targets

---
+++ EngineCore/src/EngineCore/Utils/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "EngineCore/Utils/test.hpp"
+
+namespace Engine {
+
+    void sayHello()
+    {
+        std::cout << "Hello from Engine Core" << std::endl;
+    }
+
+}
---
+++ EngineEditor/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <EngineCore/Utils/test.hpp>
+
+int main() 
+{
+	std::cout << "Hello from Engine Editor" << std::endl;
+
+	Engine::sayHello();
+
+	std::cin.get();
+}
