a44d3a456be447fc4a81d10baaa586fd21e96276
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-12 12:29:42 +0300

    added IndexBuffer class

--- EngineCore/src/EngineCore/Rendering/OpenGL/VertexBuffer.cpp
+++ EngineCore/src/EngineCore/Rendering/OpenGL/VertexBuffer.cpp
@@ -10,21 +10,21 @@ namespace Engine {
     {
         switch (type)
         {
-        case ShaderDataType::Float:
-        case ShaderDataType::Int:
-            return 1;
+            case ShaderDataType::Float:
+            case ShaderDataType::Int:
+                return 1;
 
-        case ShaderDataType::Float2:
-        case ShaderDataType::Int2:
-            return 2;
+            case ShaderDataType::Float2:
+            case ShaderDataType::Int2:
+                return 2;
 
-        case ShaderDataType::Float3:
-        case ShaderDataType::Int3:
-            return 3;
+            case ShaderDataType::Float3:
+            case ShaderDataType::Int3:
+                return 3;
 
-        case ShaderDataType::Float4:
-        case ShaderDataType::Int4:
-            return 4;
+            case ShaderDataType::Float4:
+            case ShaderDataType::Int4:
+                return 4;
         }
 
         LOG_ERROR("shader_data_type_to_component_type: unknown ShaderDataType!");
@@ -36,17 +36,17 @@ namespace Engine {
     {
         switch (type)
         {
-        case ShaderDataType::Float:
-        case ShaderDataType::Float2:
-        case ShaderDataType::Float3:
-        case ShaderDataType::Float4:
-            return sizeof(GLfloat) * shader_data_type_to_components_count(type);
+            case ShaderDataType::Float:
+            case ShaderDataType::Float2:
+            case ShaderDataType::Float3:
+            case ShaderDataType::Float4:
+                return sizeof(GLfloat) * shader_data_type_to_components_count(type);
 
-        case ShaderDataType::Int:
-        case ShaderDataType::Int2:
-        case ShaderDataType::Int3:
-        case ShaderDataType::Int4:
-            return sizeof(GLint) * shader_data_type_to_components_count(type);
+            case ShaderDataType::Int:
+            case ShaderDataType::Int2:
+            case ShaderDataType::Int3:
+            case ShaderDataType::Int4:
+                return sizeof(GLint) * shader_data_type_to_components_count(type);
         }
 
         LOG_ERROR("shader_data_type_size: unknown ShaderDataType!");
@@ -58,17 +58,17 @@ namespace Engine {
     {
         switch (type)
         {
-        case ShaderDataType::Float:
-        case ShaderDataType::Float2:
-        case ShaderDataType::Float3:
-        case ShaderDataType::Float4:
-            return GL_FLOAT;
+            case ShaderDataType::Float:
+            case ShaderDataType::Float2:
+            case ShaderDataType::Float3:
+            case ShaderDataType::Float4:
+                return GL_FLOAT;
 
-        case ShaderDataType::Int:
-        case ShaderDataType::Int2:
-        case ShaderDataType::Int3:
-        case ShaderDataType::Int4:
-            return GL_INT;
+            case ShaderDataType::Int:
+            case ShaderDataType::Int2:
+            case ShaderDataType::Int3:
+            case ShaderDataType::Int4:
+                return GL_INT;
         }
 
         LOG_ERROR("shader_data_type_to_component_type: unknown ShaderDataType!");
@@ -80,15 +80,14 @@ namespace Engine {
     {
         switch (usage)
         {
-        case VertexBuffer::EUsage::Static:  return GL_STATIC_DRAW;
-        case VertexBuffer::EUsage::Dynamic: return GL_DYNAMIC_DRAW;
-        case VertexBuffer::EUsage::Stream:  return GL_STREAM_DRAW;
+            case VertexBuffer::EUsage::Static:  return GL_STATIC_DRAW;
+            case VertexBuffer::EUsage::Dynamic: return GL_DYNAMIC_DRAW;
+            case VertexBuffer::EUsage::Stream:  return GL_STREAM_DRAW;
         }
 
         LOG_ERROR("Unknown VertexBuffer usage");
         return GL_STREAM_DRAW;
     }
-
 
     BufferElement::BufferElement(const ShaderDataType _type)
         : type(_type)
