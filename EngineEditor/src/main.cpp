20c2678de1f7828f238c940427557a053558d5d1
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-30 15:50:09 +0300

    refactoring, editor

--- EngineEditor/src/main.cpp
+++ EngineEditor/src/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
-
 #include <memory>
+#include <imgui/imgui.h>
 
 #include <EngineCore/Application.hpp>
 
-class MyApp : public Engine::Application
+class EngineEditor : public Engine::Application
 {
     virtual void on_update() override
     {
     //   std::cout << "Update frame: " << frame++ << std::endl;
+    }
+
+    virtual void on_ui_draw() override
+    {
+        ImGui::Begin("Editor");
+        ImGui::SliderFloat3("camera position", camera_position, -10.f, 10.f);
+        ImGui::SliderFloat3("camera rotation", camera_rotation, 0, 360.f);
+        ImGui::Checkbox("Perspective camera", &perspective_camera);
+        ImGui::End();
     }
 
     int frame = 0;
@@ -16,11 +25,11 @@ class MyApp : public Engine::Application
 
 int main() 
 {
-    auto myApp = std::make_unique<MyApp>();
+    auto pEngineEditor = std::make_unique<EngineEditor>();
 
-    int returnCode = myApp->start(1024, 768, "My first App");
+    int returnCode = pEngineEditor->start(1024, 768, "Engine Editor");
 
-	std::cin.get();
+	//std::cin.get();
 
     return returnCode;
 }
