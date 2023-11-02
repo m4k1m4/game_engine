486d7595190114ea34b1c9a56165c6828a714463
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-02 11:39:22 +0300

    added camera euler rotations

--- EngineCore/includes/EngineCore/Application.hpp
+++ EngineCore/includes/EngineCore/Application.hpp
@@ -27,7 +27,7 @@ namespace Engine {
         float camera_position[3] = { 0.f, 0.f, 1.f };
         float camera_rotation[3] = { 0.f, 0.f, 0.f };
         bool perspective_camera = true;
-        Camera camera;
+        Camera camera{glm::vec3(-5.f, 0.f, 0.f)};
 
     private:
         std::unique_ptr<class Window> m_pWindow;
